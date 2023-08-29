fe943ee5ccdfcd83b8429cb58fbf70bfc69d2cda
Eladia <eladianunez13@gmail.com>
2023-08-28 22:16:22 -0700

    Adding team section and making comments on CSS

--- JS/script.js
+++ JS/script.js
@@ -71,9 +71,42 @@
         }
     });
 
+
+    //Loading screen
+
     $(window).on("load", function () {
         $(".loader-wrapper").fadeOut("slow");
     });
+
+    var carousels = function () {
+        $(".owl-carousel1").owlCarousel({
+            loop: true,
+            center: true,
+            margin: 0,
+            responsiveClass: true,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
+                },
+                680: {
+                    items: 2,
+                    nav: false,
+                    loop: false
+                },
+                1000: {
+                    items: 3,
+                    nav: true
+                }
+            }
+        });
+    };
+
+    (function ($) {
+        carousels();
+
+    })();
 
 
 
